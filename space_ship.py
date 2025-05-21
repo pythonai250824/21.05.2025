30b3677c961d13702074e399723645e50fe8b40b
pythonai250824 <pythonai250824@gmail.com>
2025-05-21 20:46:18 +0300

    upload

---
+++ space_ship.py
@@ -0,0 +1,22 @@
+
+
+# class EvilSpaceShip
+# __init__(self, life=100):
+#   __life + getter/setter
+# class variables: __armor = 50
+# @classmethod - upgrade_10perc(cls) [increase armor by 10%]
+# @staticmethod - blowup_animation [print('EvilSpaceShip blow up')]
+# @classmethod - get_armor -- returns the __armor value
+
+# create 2 evil-space-ship
+# print the armor level
+# call upgrade_10perc
+# print the armor level
+# call blowup_animation
+
+# -- Order:
+# class variables
+# __init__
+# @classmethod
+# @staticmethod
+# instance-method
